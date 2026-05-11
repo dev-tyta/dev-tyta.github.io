0c80fc9497365811d06e8492f5c1b076ea6839f9
Testimony Adekoya <hardeykoryar7@gmail.com>
2026-05-11 02:41:42 +0100

    fix: cors error

--- blog-api/main.py
+++ blog-api/main.py
@@ -48,6 +48,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:8000",
         "http://localhost:8000",
+        "https://dev-tyta.github.io/"
         "null",  # Dev only: allows file:// protocol access
     ],
     allow_credentials=True,
